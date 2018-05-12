10a9aea84f9cc50ee884ff11047119b725d3fc2b
muthuramansv <muthu_ramansv@hotmail.com>
2018-05-12 11:13:49 +0200

    Update spec.js.js

--- spec.js.js
+++ spec.js.js
@@ -1,22 +1,13 @@
-(function () {
-    function checkTime(i) {
-        return (i < 10) ? "0" + i : i;
+var fs = require('fs');
+
+var date = new Date().toISOString().
+replace(/T/, ' ').
+replace(/\..+/, '')
+
+fs.writeFile("views/Output.txt", date, function(err) {
+    if(err) {
+        return console.log(err);
     }
+    console.log("File is Successfully Created");
 
-    function startTime() {
-        var today = new Date(),
-
-            day = checkTime(today.getDate()),
-            month = checkTime(today.getMonth()),
-            year = checkTime(today.getYear()),
-            h = checkTime(today.getHours()),
-            m = checkTime(today.getMinutes()),
-            s = checkTime(today.getSeconds());
-        document.getElementById('time').innerHTML = day + "-" + month + "-" + year + "....." + h + ":" + m + ":" + s;
-        t = setTimeout(function () {
-            startTime()
-        }, 500);
-    }
-    startTime();
-})();
-
+});
